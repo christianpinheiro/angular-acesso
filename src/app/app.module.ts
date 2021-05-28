816d9f8271c292b88100459eff10f75d16c5fb5f
christianpinheiro <93pinheiro@gmail.com>
2021-05-28 17:08:33 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -20,7 +21,8 @@ import { HelloComponent } from './hello.component';
         messagingSenderId: "961970735176",
         appId: "1:961970735176:web:fab9493c21aa3e20dcb07d"
       }
-    )
+    ),
+    AngularFireAuthModule
   ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
