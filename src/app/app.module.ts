5e47c5c08057159acbf73ce488e2788626ba526f
christianpinheiro <93pinheiro@gmail.com>
2021-05-28 17:06:24 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from '@angular/fire';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    AngularFireModule.initializeApp(
+      {
+        apiKey: "AIzaSyBZrfPb7VF--tPth13bPbZ1wTZ_30RXr20",
+        authDomain: "angularcontroleacesso.firebaseapp.com",
+        projectId: "angularcontroleacesso",
+        storageBucket: "angularcontroleacesso.appspot.com",
+        messagingSenderId: "961970735176",
+        appId: "1:961970735176:web:fab9493c21aa3e20dcb07d"
+      }
+    )
+  ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
