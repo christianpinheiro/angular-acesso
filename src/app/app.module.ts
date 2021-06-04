39e5c33c065ce43691b9b94400c29ecb3408256b
christianpinheiro <93pinheiro@gmail.com>
2021-06-04 17:12:42 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,23 +10,21 @@ import { HelloComponent } from './hello.component';
 import { AuthService } from './auth.service';
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
+  imports: [
+    BrowserModule,
     FormsModule,
-    AngularFireModule.initializeApp(
-      {
-        apiKey: "AIzaSyBZrfPb7VF--tPth13bPbZ1wTZ_30RXr20",
-        authDomain: "angularcontroleacesso.firebaseapp.com",
-        projectId: "angularcontroleacesso",
-        storageBucket: "angularcontroleacesso.appspot.com",
-        messagingSenderId: "961970735176",
-        appId: "1:961970735176:web:fab9493c21aa3e20dcb07d"
-      }
-    ),
-    AngularFireAuthModule,
-    ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ],
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBZrfPb7VF--tPth13bPbZ1wTZ_30RXr20',
+      authDomain: 'angularcontroleacesso.firebaseapp.com',
+      projectId: 'angularcontroleacesso',
+      storageBucket: 'angularcontroleacesso.appspot.com',
+      messagingSenderId: '961970735176',
+      appId: '1:961970735176:web:fab9493c21aa3e20dcb07d'
+    }),
+    AngularFireAuthModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
   providers: [AuthService]
 })
-export class AppModule { }
+export class AppModule {}
