dc4a58289d12492e9af2f939aabf23651802afcc
christianpinheiro <93pinheiro@gmail.com>
2021-06-04 17:54:15 -0300

    1 file modified

--- src/app/firelist/firelist.component.ts
+++ src/app/firelist/firelist.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
-import { Observable } from 'rxjs/dist/types';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-firelist',
