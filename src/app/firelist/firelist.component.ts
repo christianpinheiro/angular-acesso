92c2486357ff5c0d3650a69c7ceb053c64e2f83c
christianpinheiro <93pinheiro@gmail.com>
2021-06-04 17:45:21 -0300

    2 files modified

--- src/app/firelist/firelist.component.ts
+++ src/app/firelist/firelist.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./firelist.component.css']
 })
 export class FirelistComponent implements OnInit {
-
+  text: string;
+  
   constructor() { }
 
   ngOnInit() {
